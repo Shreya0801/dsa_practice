2bf94da50a745b408cca6fb5f0e781e55c0b2aca
Shreya <shreyasingh85301@gmail.com>
2024-12-21 22:50:07 +0530

    modify the file by calculating avg of three num

--- largest_num.java
+++ largest_num.java
@@ -24,6 +24,9 @@ public class largest_num {
         }
 
         System.out.println("The largest number is: " + largest);
+
+        double average = (num1 + num2 + num3) / 3.0;
+        System.out.println("The average of the three numbers is: " + average);
         scanner.close();
     }
 }
